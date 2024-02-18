78fb6466fb527dc091942b6d486761c82ad02a26
Sage Wall <swall@esri.com>
2024-02-18 11:35:37 -0700

    chore: inital commit

---
+++ src/main.ts
@@ -0,0 +1,154 @@
+import Color from "@arcgis/core/Color";
+import Map from "@arcgis/core/Map";
+import GeoJSONLayer from "@arcgis/core/layers/GeoJSONLayer";
+import SimpleRenderer from "@arcgis/core/renderers/SimpleRenderer";
+import OpacityVariable from "@arcgis/core/renderers/visualVariables/OpacityVariable";
+import SimpleFillSymbol from "@arcgis/core/symbols/SimpleFillSymbol";
+import SimpleLineSymbol from "@arcgis/core/symbols/SimpleLineSymbol";
+import MapView from "@arcgis/core/views/MapView";
+import Expand from "@arcgis/core/widgets/Expand";
+import LayerList from "@arcgis/core/widgets/LayerList";
+import { defineCustomElements } from "@esri/calcite-components/dist/loader";
+import "./style.css";
+import CSVLayer from "@arcgis/core/layers/CSVLayer";
+import SimpleMarkerSymbol from "@arcgis/core/symbols/SimpleMarkerSymbol";
+
+defineCustomElements(window, {
+  resourcesUrl: "https://js.arcgis.com/calcite-components/2.4.0/assets",
+});
+setUp();
+
+// Create a GeoJSON layers for the eclipse
+const centerLayer = new GeoJSONLayer({
+  renderer: new SimpleRenderer({
+    symbol: new SimpleLineSymbol({
+      color: "red",
+      width: 2,
+    }),
+  }),
+  title: "Center",
+  url: "./data/center.geojson",
+});
+
+const penumbraLayer = new GeoJSONLayer({
+  renderer: new SimpleRenderer({
+    symbol: new SimpleFillSymbol({
+      color: new Color({
+        r: 0,
+        g: 0,
+        b: 0,
+      }),
+      outline: {
+        color: new Color({
+          r: 0,
+          g: 0,
+          b: 0,
+          a: 0.25,
+        }),
+        width: 0.5,
+      },
+    }),
+    visualVariables: [
+      new OpacityVariable({
+        field: "Obscuratio",
+        stops: [
+          { value: 100, opacity: 50 },
+          { value: 0, opacity: 0 },
+        ],
+      }),
+    ],
+  }),
+  title: "Penumbra",
+  url: "./data/penumbra.geojson",
+});
+
+const totalityLayer = new GeoJSONLayer({
+  renderer: new SimpleRenderer({
+    symbol: new SimpleFillSymbol({
+      color: new Color({
+        r: 0,
+        g: 0,
+        b: 0,
+        a: 0.5,
+      }),
+      outline: {
+        color: new Color({
+          r: 0,
+          g: 0,
+          b: 0,
+          a: 0.5,
+        }),
+        width: 1,
+      },
+    }),
+  }),
+  title: "Totality",
+  url: "./data/totality.geojson",
+});
+
+// Create a CSVLayer for april cloud cover in various cities
+const cloudCoverLayer = new CSVLayer({
+  title: "April Cloud Cover",
+  url: "./data/cloud-cover.csv",
+});
+
+// Create a CSVLayer for festivals
+const festivalsLayer = new CSVLayer({
+  renderer: new SimpleRenderer({
+    symbol: new SimpleMarkerSymbol({}),
+  }),
+  title: "Festivals",
+  url: "./data/festivals.csv",
+});
+
+const map = new Map({
+  layers: [penumbraLayer, totalityLayer, cloudCoverLayer, festivalsLayer, centerLayer],
+  basemap: "streets-vector",
+});
+
+const view = new MapView({
+  container: "viewDiv",
+  map,
+  zoom: 4,
+  center: [-85, 35],
+});
+
+// Create a LayerList widget and add it to the view's UI
+const layerList = new LayerList({
+  view,
+});
+const layerListExpand = new Expand({
+  view,
+  content: layerList,
+});
+
+view.ui.add(layerListExpand, "top-left");
+
+// Set up the user interface
+function setUp() {
+  const toggleModalEl = document.getElementById("toggle-modal") as HTMLCalciteActionElement;
+  const navigationEl = document.getElementById("nav") as HTMLCalciteNavigationElement;
+  const panelEl = document.getElementById("sheet-panel") as HTMLCalcitePanelElement;
+  const modalEl = document.getElementById("modal") as HTMLCalciteModalElement;
+  const sheetEl = document.getElementById("sheet") as HTMLCalciteSheetElement;
+
+  toggleModalEl?.addEventListener("click", () => handleModalChange());
+  navigationEl?.addEventListener("calciteNavigationActionSelect", () => handleSheetOpen());
+
+  panelEl?.addEventListener("calcitePanelClose", () => handlePanelClose());
+
+  function handleModalChange() {
+    if (modalEl) {
+      modalEl.open = !modalEl.open;
+    }
+  }
+
+  function handleSheetOpen() {
+    sheetEl.open = true;
+    panelEl.closed = false;
+  }
+
+  function handlePanelClose() {
+    sheetEl.open = false;
+  }
+}
