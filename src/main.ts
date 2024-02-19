f34010c4fb4da3add30e9bc41fa580bd04415365
Sage Wall <sagewall@users.noreply.github.com>
2024-02-19 06:28:37 -0700

    feat: add city times layer

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,7 @@ import LabelClass from "@arcgis/core/layers/support/LabelClass";
 import ClassBreaksRenderer from "@arcgis/core/renderers/ClassBreaksRenderer";
 import SimpleRenderer from "@arcgis/core/renderers/SimpleRenderer";
 import OpacityVariable from "@arcgis/core/renderers/visualVariables/OpacityVariable";
+import esriRequest from "@arcgis/core/request";
 import SimpleFillSymbol from "@arcgis/core/symbols/SimpleFillSymbol";
 import SimpleLineSymbol from "@arcgis/core/symbols/SimpleLineSymbol";
 import SimpleMarkerSymbol from "@arcgis/core/symbols/SimpleMarkerSymbol";
@@ -14,162 +15,17 @@ import MapView from "@arcgis/core/views/MapView";
 import Expand from "@arcgis/core/widgets/Expand";
 import LayerList from "@arcgis/core/widgets/LayerList";
 import { defineCustomElements } from "@esri/calcite-components/dist/loader";
+import type { GeoJSON } from "geojson";
 import "./style.css";
 import { cloudSymbol, sunSymbol } from "./symbols";
+import type { CityTimes } from "./types";
 
 defineCustomElements(window, {
   resourcesUrl: "https://js.arcgis.com/calcite-components/2.4.0/assets",
 });
 setUp();
 
-// Create a GeoJSON layers for the eclipse
-const centerLayer = new GeoJSONLayer({
-  renderer: new SimpleRenderer({
-    symbol: new SimpleLineSymbol({
-      color: "red",
-      width: 2,
-    }),
-  }),
-  title: "Center",
-  url: "./data/center.geojson",
-});
-
-const penumbraLayer = new GeoJSONLayer({
-  renderer: new SimpleRenderer({
-    symbol: new SimpleFillSymbol({
-      color: new Color({
-        r: 0,
-        g: 0,
-        b: 0,
-      }),
-      outline: {
-        color: new Color({
-          r: 0,
-          g: 0,
-          b: 0,
-          a: 0.25,
-        }),
-        width: 0.5,
-      },
-    }),
-    visualVariables: [
-      new OpacityVariable({
-        field: "Obscuratio",
-        stops: [
-          { value: 100, opacity: 50 },
-          { value: 0, opacity: 0 },
-        ],
-      }),
-    ],
-  }),
-  title: "Penumbra",
-  url: "./data/penumbra.geojson",
-  visible: false,
-});
-
-const durationLayer = new GeoJSONLayer({
-  renderer: new SimpleRenderer({
-    symbol: new SimpleFillSymbol({
-      color: new Color({
-        r: 0,
-        g: 0,
-        b: 0,
-        a: 0,
-      }),
-      outline: {
-        color: new Color({
-          r: 100,
-          g: 100,
-          b: 100,
-          a: 1,
-        }),
-        width: 0.5,
-      },
-    }),
-  }),
-  title: "Duration",
-  url: "./data/duration.geojson",
-  visible: false,
-});
-
-const totalityLayer = new GeoJSONLayer({
-  blendMode: "multiply",
-  renderer: new SimpleRenderer({
-    symbol: new SimpleFillSymbol({
-      color: new Color({
-        r: 200,
-        g: 200,
-        b: 200,
-      }),
-      outline: {
-        color: new Color({
-          r: 100,
-          g: 100,
-          b: 100,
-        }),
-        width: 1,
-      },
-    }),
-  }),
-  title: "Totality",
-  url: "./data/totality.geojson",
-});
-
-// Create a lable class for the cloud cover layer
-const cloudCoverLabelClass = new LabelClass({
-  symbol: new TextSymbol({
-    color: "white",
-    font: {
-      size: 12,
-      weight: "bold",
-    },
-    haloColor: "black",
-    haloSize: 1,
-    xoffset: 10,
-    yoffset: 10,
-  }),
-  labelPlacement: "center-left",
-  labelExpressionInfo: {
-    expression: "Text(Round(($feature.APRIL_CLEAR_DAYS / 30)*100, 0)) + '%'",
-  },
-  minScale: 5000000,
-});
-
-// Create a CSVLayer for april cloud cover in various cities
-const cloudCoverLayer = new CSVLayer({
-  labelingInfo: [cloudCoverLabelClass],
-  opacity: 0.8,
-  renderer: new ClassBreaksRenderer({
-    classBreakInfos: [
-      {
-        minValue: 0,
-        maxValue: 14,
-        symbol: cloudSymbol,
-      },
-      {
-        minValue: 15,
-        maxValue: 30,
-        symbol: sunSymbol,
-      },
-    ],
-    defaultSymbol: sunSymbol,
-    field: "APRIL_CLEAR_DAYS",
-  }),
-  title: "Chance of Sunny Skies in April",
-  url: "./data/cloud-cover.csv",
-});
-
-// Create a CSVLayer for festivals
-const festivalsLayer = new CSVLayer({
-  renderer: new SimpleRenderer({
-    symbol: new SimpleMarkerSymbol({}),
-  }),
-  title: "Festivals",
-  url: "./data/festivals.csv",
-});
-
 const map = new Map({
-  layers: [penumbraLayer, totalityLayer, durationLayer, festivalsLayer, centerLayer, cloudCoverLayer],
   basemap: "streets-vector",
 });
 
@@ -193,6 +49,166 @@ view.ui.add(layerListExpand, "top-left");
 
 // Create popup templates for each layer
 view.when(async () => {
+  // Create a GeoJSON layers for the eclipse
+  const centerLayer = new GeoJSONLayer({
+    renderer: new SimpleRenderer({
+      symbol: new SimpleLineSymbol({
+        color: "red",
+        width: 2,
+      }),
+    }),
+    title: "Center",
+    url: "./data/center.geojson",
+  });
+
+  const durationLayer = new GeoJSONLayer({
+    renderer: new SimpleRenderer({
+      symbol: new SimpleFillSymbol({
+        color: new Color({
+          r: 0,
+          g: 0,
+          b: 0,
+          a: 0,
+        }),
+        outline: {
+          color: new Color({
+            r: 100,
+            g: 100,
+            b: 100,
+            a: 1,
+          }),
+          width: 0.5,
+        },
+      }),
+    }),
+    title: "Duration",
+    url: "./data/duration.geojson",
+    visible: false,
+  });
+
+  const penumbraLayer = new GeoJSONLayer({
+    renderer: new SimpleRenderer({
+      symbol: new SimpleFillSymbol({
+        color: new Color({
+          r: 0,
+          g: 0,
+          b: 0,
+        }),
+        outline: {
+          color: new Color({
+            r: 0,
+            g: 0,
+            b: 0,
+            a: 0.25,
+          }),
+          width: 0.5,
+        },
+      }),
+      visualVariables: [
+        new OpacityVariable({
+          field: "Obscuratio",
+          stops: [
+            { value: 100, opacity: 50 },
+            { value: 0, opacity: 0 },
+          ],
+        }),
+      ],
+    }),
+    title: "Penumbra",
+    url: "./data/penumbra.geojson",
+    visible: false,
+  });
+
+  const totalityLayer = new GeoJSONLayer({
+    blendMode: "multiply",
+    renderer: new SimpleRenderer({
+      symbol: new SimpleFillSymbol({
+        color: new Color({
+          r: 200,
+          g: 200,
+          b: 200,
+        }),
+        outline: {
+          color: new Color({
+            r: 100,
+            g: 100,
+            b: 100,
+          }),
+          width: 1,
+        },
+      }),
+    }),
+    title: "Totality",
+    url: "./data/totality.geojson",
+  });
+
+  // Create a GeoJSON layer for the cities and their eclipse times
+  const cityTimesLayer = await createCityTimesLayer();
+
+  // Create a lable class for the cloud cover layer
+  const cloudCoverLabelClass = new LabelClass({
+    symbol: new TextSymbol({
+      color: "white",
+      font: {
+        size: 12,
+        weight: "bold",
+      },
+      haloColor: "black",
+      haloSize: 1,
+      xoffset: 10,
+      yoffset: 10,
+    }),
+    labelPlacement: "center-left",
+    labelExpressionInfo: {
+      expression: "Text(Round(($feature.APRIL_CLEAR_DAYS / 30)*100, 0)) + '%'",
+    },
+    minScale: 5000000,
+  });
+
+  // Create a CSVLayer for april cloud cover in various cities
+  const cloudCoverLayer = new CSVLayer({
+    labelingInfo: [cloudCoverLabelClass],
+    opacity: 0.8,
+    renderer: new ClassBreaksRenderer({
+      classBreakInfos: [
+        {
+          minValue: 0,
+          maxValue: 14,
+          symbol: cloudSymbol,
+        },
+        {
+          minValue: 15,
+          maxValue: 30,
+          symbol: sunSymbol,
+        },
+      ],
+      defaultSymbol: sunSymbol,
+      field: "APRIL_CLEAR_DAYS",
+    }),
+    title: "Chance of Sunny Skies in April",
+    url: "./data/cloud-cover.csv",
+  });
+
+  // Create a CSVLayer for festivals
+  const festivalsLayer = new CSVLayer({
+    renderer: new SimpleRenderer({
+      symbol: new SimpleMarkerSymbol({}),
+    }),
+    title: "Festivals",
+    url: "./data/festivals.csv",
+  });
+
+  map.addMany([
+    cityTimesLayer,
+    cloudCoverLayer,
+    centerLayer,
+    durationLayer,
+    festivalsLayer,
+    penumbraLayer,
+    totalityLayer,
+  ]);
+
+  await cityTimesLayer.load();
   await centerLayer.load();
   await cloudCoverLayer.load();
   await durationLayer.load();
@@ -200,6 +216,7 @@ view.when(async () => {
   await penumbraLayer.load();
   await totalityLayer.load();
 
+  cityTimesLayer.popupTemplate = cityTimesLayer.createPopupTemplate();
   centerLayer.popupTemplate = centerLayer.createPopupTemplate();
   cloudCoverLayer.popupTemplate = cloudCoverLayer.createPopupTemplate();
   durationLayer.popupTemplate = durationLayer.createPopupTemplate();
@@ -207,6 +224,48 @@ view.when(async () => {
   penumbraLayer.popupTemplate = penumbraLayer.createPopupTemplate();
   totalityLayer.popupTemplate = totalityLayer.createPopupTemplate();
 });
+
+async function createCityTimesLayer(): Promise<GeoJSONLayer> {
+  const response = await esriRequest("./data/city-times.json", {
+    responseType: "json",
+  });
+
+  const geoJSON: GeoJSON = {
+    type: "FeatureCollection",
+    features: [],
+  };
+
+  response.data.forEach((city: CityTimes) => {
+    geoJSON.features.push({
+      type: "Feature",
+      geometry: {
+        type: "Point",
+        coordinates: [city.LON, city.LAT],
+      },
+      properties: {
+        name: city.NAME,
+        state: city.STATE,
+        t1: city.ECLIPSE[1],
+        t2: city.ECLIPSE[2],
+        t3: city.ECLIPSE[3],
+        t4: city.ECLIPSE[4],
+        t5: city.ECLIPSE[5],
+      },
+    });
+  });
+
+  const blob = new Blob([JSON.stringify(geoJSON)], {
+    type: "application/json",
+  });
+
+  const url = URL.createObjectURL(blob);
+
+  const cityTimes = new GeoJSONLayer({
+    url,
+  });
+
+  return cityTimes;
+}
 
 // Set up the user interface
 function setUp() {
---
+++ src/types.ts
@@ -0,0 +1,7 @@
+export type CityTimes = {
+  ECLIPSE: number[];
+  LAT: number;
+  LON: number;
+  NAME: string;
+  STATE: string;
+};
